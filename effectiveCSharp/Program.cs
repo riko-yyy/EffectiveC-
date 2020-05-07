8e8232f314c2e76c031f57819fd50bfc812a0c37
riko <c.c.3yegk1hass.w1s0vhd@gmail.com>
2020-05-07 20:36:32 +0900

    first commit

---
+++ effectiveCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace effectiveCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

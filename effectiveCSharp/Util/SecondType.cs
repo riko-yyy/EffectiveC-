c9c0b4d62126d5924ec2609ff7b16ec2bce4cdef
riko <c.c.3yegk1hass.w1s0vhd@gmail.com>
2020-05-08 23:55:57 +0900

    No.3

---
+++ effectiveCSharp/Util/SecondType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace effectiveCSharp.Util
+{
+    public class SecondType
+    {
+        private MyType _value;
+
+        public SecondType()
+        {
+        }
+
+        /// <summary>
+        /// ユーザー定義の変換演算子
+        /// </summary>
+        /// <param name="t"></param>
+        public static implicit operator MyType(SecondType t)
+        {
+            return t._value;
+        }
+
+    }
+}

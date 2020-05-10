264af18ecf5095697d62eca8afe91aefb60a13b0
riko <c.c.3yegk1hass.w1s0vhd@gmail.com>
2020-05-10 15:49:50 +0900

    No.8

---
+++ effectiveCSharp/No08_UseNullConditionalOperatorWhenCallEvent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using effectiveCSharp.Util;
+namespace effectiveCSharp
+{
+    /// <summary>
+    /// イベント呼び出し時にnull条件演算子を使う
+    /// </summary>
+    public class No08_UseNullConditionalOperatorWhenCallEvent
+    {
+        public No08_UseNullConditionalOperatorWhenCallEvent()
+        {
+            //目的：
+            //①スレッドセーフな記述
+            //②可読性、冗長なヘルパーメソッドの作成の回避
+
+            //例：
+            //--------------------------------------------------------------------------------------
+            //1)スレッドセーフな記述
+
+            //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+            //メリット①：１行で簡単に記述できる
+
+            //実装はeffectiveCSharp.Util.EventSourceを参照
+
+            //使う時はこんな感じ
+            //メソッドをイベントハンドラー化
+            var aaa = new EventHandler<int>(newFunc);
+
+            //イベント待ち受けクラスをインスタンス化
+            //デリゲート登録
+            EventSource eventSource = new EventSource(aaa);
+
+            //登録したデリゲードを実行
+            eventSource.RaiseUpdates();
+
+        }
+
+        //実際に動かさせたいメソッド
+        private void newFunc(object sender, int e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
---
+++ effectiveCSharp/Util/EventSource.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace effectiveCSharp.Util
+{
+    /// <summary>
+    /// イベント発生待ち受けクラス
+    /// 外から実行する動作をデリゲート（イベントハンドラー）経由で登録しておき、RaiseUpdates()が実行されたタイミングで実行
+    /// </summary>
+    public class EventSource
+    {
+        //デリゲート格納
+        private EventHandler<int> Updated;
+
+        //呼び出し回数
+        private int counter;
+
+        /// <summary>
+        /// コンストラクター
+        /// </summary>
+        /// <param name="func"></param>
+        public EventSource(EventHandler<int> func)
+        {
+            Updated = func;
+        }
+
+        /// <summary>
+        /// カウンターを加算して、そのカウンターを引数にデリゲード実行
+        /// </summary>
+        public void RaiseUpdates()
+        {
+            counter++;
+
+            //×：イベントハンドラに値がない状態で実行するとNullReferenceException
+            Updated(this, counter);
+
+            //×：nullの検証後に別スレッドでデリゲートの削除が生じるとNullReferenceException
+            if (Updated != null)
+            {
+                Updated(this, counter);
+            }
+
+            //△：検証後にデリゲート削除が生じても、別の変数に複製してるから影響を受けない
+            //   ただし冗長で可読性にかける、一見して意味がわかりにくい
+            var handler = Updated;
+            if (handler != null)
+            {
+                handler(this, counter);
+            }
+
+            //○：null条件演算子を使用。nullなら実行しないし、nullでないなら実行
+            //  検証＆実行も厳密に同じタイミング
+            Updated?.Invoke(this, counter);
+
+        }
+
+
+    }
+}

fb8d082669d0697d0c91740bc2d2255e33406ac6
riko <c.c.3yegk1hass.w1s0vhd@gmail.com>
2020-05-19 00:38:13 +0900

    No.13

---
+++ effectiveCSharp/Util/MySingleton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace effectiveCSharp.Util
+{
+    public class MySingleton
+    {
+        //オブジェクト初期化子で初期化
+        private static readonly MySingleton theOneAndOnly = new MySingleton();
+
+        //外からstaticメンバを取得する用のpublicメソッド
+        public static MySingleton TheOnly { get { return theOneAndOnly; } }
+
+        //通常コンストラクタをprivate化
+        private MySingleton()
+        {
+        }
+    }
+}
---
+++ effectiveCSharp/Util/MySingleton2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace effectiveCSharp.Util
+{
+    public class MySingleton2
+    {
+        //staticコンストラクタで初期化
+        private static readonly MySingleton2 theOneAndOnly;        
+        static MySingleton2()
+        {
+            theOneAndOnly = new MySingleton2();
+        }
+
+        //外からstaticメンバを取得する用のpublicメソッド
+        public static MySingleton2 TheOnly { get { return theOneAndOnly; } }
+
+        //通常コンストラクタをprivate化
+        private MySingleton2()
+        {
+        }
+    }
+}

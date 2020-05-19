9996bdd6dd73b4fddd3d2f5d05c4a653eea9951e
riko <c.c.3yegk1hass.w1s0vhd@gmail.com>
2020-05-20 00:18:18 +0900

    No.14

---
+++ effectiveCSharp/Util/MyClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace effectiveCSharp.Util
+{
+    public class MyClass
+    {
+        private List<int> coll;
+        private readonly string name;
+
+        //これ3つめのコンストラクタを呼び出し
+        public MyClass() : this(0, "")
+        {
+        }
+
+        //これ3つめのコンストラクタを呼び出し
+        public MyClass(int initialCount) : this(initialCount, string.Empty)
+        {
+        }
+
+        public MyClass(int initialCount, string name)
+        {
+            coll = (initialCount > 0) ? new List<int>(initialCount) : new List<int>();
+            this.name = name;
+        }
+
+
+    }
+}
---
+++ effectiveCSharp/Util/MyClass2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace effectiveCSharp.Util
+{
+    public class MyClass2
+    {
+        private List<int> coll;
+        private string name;
+
+        //これ２つめのコンストラクタを呼び出し
+        //ジェネリクス中で利用可能にするため、newを使えるように引数なしコンストラクタは必要
+        public MyClass2() : this(0, string.Empty)
+        {
+        }
+
+        //デフォルト引数の用意でさらにオーバーロードコンストラクタを少なくできる
+        public MyClass2(int initialCount = 0, string name = "")
+        {
+            coll = (initialCount > 0) ? new List<int>(initialCount) : new List<int>();
+            this.name = name;
+        }
+    }
+}

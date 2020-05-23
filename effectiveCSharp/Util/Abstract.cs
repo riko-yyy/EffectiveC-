744b1e25adc07bed1afc6f3e1f524727ccdf562b
riko <c.c.3yegk1hass.w1s0vhd@gmail.com>
2020-05-23 18:43:41 +0900

    No.16

---
+++ effectiveCSharp/Util/Abstract.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace effectiveCSharp.Util
+{
+    public class Abstract
+    {
+        protected Abstract()
+        {
+            Func();
+        }
+
+        protected virtual void Func()
+        {
+            Console.WriteLine("親だよ");
+        }
+
+    }
+}
---
+++ effectiveCSharp/Util/Derived.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace effectiveCSharp.Util
+{
+    public class Derived : Abstract
+    {
+        private readonly string msg = "子の初期化子だよ";
+
+        public Derived(string msg)
+        {
+            this.msg = msg;
+        }
+
+        protected override void Func()
+        {
+            Console.WriteLine(msg);
+        }
+
+        public static void MainDerived()
+        {
+            var d = new Derived("子のMainコンストラクタだよ");
+        }
+    }
+}

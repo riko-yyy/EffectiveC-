890a82a114ee05350bfc1c259b5648bc6978aa86
riko <c.c.3yegk1hass.w1s0vhd@gmail.com>
2020-05-25 23:37:26 +0900

    No.18

---
+++ effectiveCSharp/No18_AlwaysDefineMinimumConstraint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using effectiveCSharp.Util;
+
+namespace effectiveCSharp
+{
+    /// <summary>
+    /// 最低限必須となる制約を常に定義すること
+    /// </summary>
+    public class No18_AlwaysDefineMinimumConstraint
+    {
+        private const string V = "aaaa";
+
+        public No18_AlwaysDefineMinimumConstraint()
+        {
+            //目的：
+            //①実行時の検証ではなくコンパイル時に検証可能
+
+            //概要：
+            //--------------------------------------------------------------------------------------
+            //ジェネリクスに型引数として渡り得る型に制約を設けることで、コンパイル時にエラーを発見する
+            //ただし、制約をかけすぎると使いにくいメソッドとなるため、最小限に止める
+
+            //利用時
+            Constraint.AreEqual(1,1);
+            Constraint.AreEqual2(1, 1);
+            Constraint.AreEqual3(1, 1);
+            Constraint.AreEqual4(1, 1);
+
+            IEnumerable<int> a = new List<int>() { 1, 2, 3 };
+            a.FirstOrDefault(v => v == 1);
+
+            Constraint.Factory<MyClass>(() => { return new MyClass(); });
+        }
+    }
+}
---
+++ effectiveCSharp/Util/Constraint.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace effectiveCSharp.Util
+{
+    public static class Constraint
+    {
+
+        //悪い例：メソッド中で利用したいメソッドが利用可能か、isを用いた検証処理が必要
+        public static bool AreEqual<T>(T left, T right)
+        {
+            //nullなら両方nullか
+            if (left == null)
+            {
+                return right == null;
+            }
+
+            //leftがIComparable<T>を実装しているか検証
+            if (left is IComparable<T>)
+            {
+                //キャスト
+                IComparable<T> lval = left as IComparable<T>;
+
+                //rightがIComparable<T>を実装しているか検証
+                if (right is IComparable<T>)
+                {
+                    return lval.CompareTo(right) == 0;
+                }
+                else
+                {
+                    throw new ArgumentException("IComparable<T>が実装されていない型です", nameof(right));
+                }
+
+            }
+            else
+            {
+                throw new ArgumentException("IComparable<T>が実装されていない型です", nameof(left));
+            }
+        }
+
+        //良い例：制約でTがIComparable<T>を実装していることを保証している
+        public static bool AreEqual2<T>(T left, T right) where T : IComparable<T>
+        {
+            return left.CompareTo(right) == 0;
+        }
+
+
+        //制約を利用するか悩む例
+        //制約なし
+        public static bool AreEqual3<T>(T left, T right) => left.Equals(right);
+        //制約あり
+        public static bool AreEqual4<T>(T left, T right) where T : IEquatable<T> => left.Equals(right);
+
+        //Tに応じて、IEquatable<T>.Equals()もしくはSystem.Object.Equals()が使用される
+        //値型の場合、ボックス化が実行されオーバーヘッドが生じ得る。
+        //回避するためにIEquatable<T>の制約するのも良いが、このメソッドを利用するクラス全てがIEquatable<T>の実装を強制される
+        //そこであえて「悪い例」のように型チェックごとに内部で利用するメソッドを変えてもOK
+
+
+
+
+        //new制約の取り扱い
+        //制約は最小限に留めたいため、避けれられるものは避けたい。
+        //デフォルトコンストラクタはdefault()によって回避できる可能性がある
+        //default
+        public static T FirstOrDefault<T>(this IEnumerable<T> sequence, Predicate<T> test)
+        {
+            foreach (T value in sequence)
+            {
+                if (test(value))
+                {
+                    return value;
+                }
+            }
+
+            return default;
+        }
+
+        //new制約
+        public delegate T FactoryFunc<T>();
+        public static T Factory<T>(FactoryFunc<T> makeANewT) where T : new()
+        {
+            T rVal = makeANewT();
+            if (rVal ==null)
+            {
+                return new T();
+            }
+            else
+            {
+                return rVal;
+            }
+
+        }
+
+    }
+}

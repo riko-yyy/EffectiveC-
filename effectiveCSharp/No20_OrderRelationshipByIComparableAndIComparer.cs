d073eea3173a09078f7f35ef46ce97703ce9174f
riko <c.c.3yegk1hass.w1s0vhd@gmail.com>
2020-05-30 19:29:08 +0900

    No.20

---
+++ effectiveCSharp/No20_OrderRelationshipByIComparableAndIComparer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using effectiveCSharp.Util;
+
+namespace effectiveCSharp
+{
+    /// <summary>
+    /// IComparable<T>とIComparer<T>により順序関係を実装する
+    /// </summary>
+    public class No20_OrderRelationshipByIComparableAndIComparer
+    {
+        public No20_OrderRelationshipByIComparableAndIComparer()
+        {
+            //目的：
+            //①コレクションをソートする際の順序関係を定義する
+
+            //概要：
+            //--------------------------------------------------------------------------------------
+            //順序関係の判断を拡張し、自作のソート機能を提供する
+
+            //利用時
+            var customers = new List<CustomerForCompare>();
+            customers.Add(new CustomerForCompare("B", 1000));
+            customers.Add(new CustomerForCompare("A", 800));
+            customers.Add(new CustomerForCompare("D", 700));
+            customers.Add(new CustomerForCompare("C", 900));
+            customers.Add(new CustomerForCompare("F", 600));
+
+            customers.ForEach(v => { Console.WriteLine($"name:{v.name},revenue:{v.revenue}"); });
+
+            //CompareTo
+            var resultNameInt = customers[0].CompareTo(customers[1]);
+
+            //CompareToを利用したオーバーロード比較演算子
+            var resultNameBool = customers[0] < customers[1];
+
+            //ソート
+            //CompareToはList<T>.Sort()やSortedList<T>.Addなどで標準的に呼ばれる
+            customers.Sort();
+            customers.ForEach(v => { Console.WriteLine($"name:{v.name},revenue:{v.revenue}"); });
+
+            //revenueで比較（自作クラス向け）
+            var resultRevenueInt1 = CustomerForCompare.CompareByRevenue(customers[0], customers[1]);
+            //revenueでソート
+            customers.Sort(CustomerForCompare.CompareByRevenue);
+            customers.ForEach(v => { Console.WriteLine($"name:{v.name},revenue:{v.revenue}"); });
+
+            //revenueで比較（.NET標準クラス向け）
+            var resultRevenueInt2 = CustomerForCompare.RevenueCompare.Compare(customers[0], customers[1]);
+            //revenueでソート
+            customers.Sort(CustomerForCompare.RevenueCompare); 
+            customers.ForEach(v => { Console.WriteLine($"name:{v.name},revenue:{v.revenue}"); });
+        }
+    }
+}
---
+++ effectiveCSharp/Util/CustomerForCompare.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace effectiveCSharp.Util
+{
+
+    /// <summary>
+    /// 顧客
+    /// </summary>
+    public class CustomerForCompare : IComparable<CustomerForCompare>, IComparable
+    {
+        /// <summary>
+        /// 氏名
+        /// </summary>
+        public string name { get; }
+
+        /// <summary>
+        /// 所得
+        /// </summary>
+        public double revenue { get; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="name"></param>
+        public CustomerForCompare(string name, double revenue)
+        {
+            this.name = name;
+            this.revenue = revenue;
+        }
+
+
+        //-----------------------------------------------IComparable<Customer>の実装
+        //インスタンスの並べ替えを目的とする型固有の汎用比較メソッドをサポートするインタフェース(ジェネリクス)
+
+        /// <summary>
+        /// 比較メソッド(ジェネリクス)
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareTo(CustomerForCompare other) => name.CompareTo(other.name);
+
+
+        //-----------------------------------------------IComparableの実装
+        //インスタンスの並べ替えを目的とする型固有の汎用比較メソッドをサポートするインタフェース(非ジェネリクス)
+
+        /// <summary>
+        /// 比較メソッド(非ジェネリクス)
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        int IComparable.CompareTo(object obj)
+        {
+            //引数がobjectでありボックス化のオーバヘッドが生じるため、なるべく使用は避けたい
+            //そこでメソッドを明示的に実装し、呼び出しも明示的でないとできないようにする
+            if (!(obj is CustomerForCompare))
+            {
+                throw new ArgumentException("引数はCustomer型ではありません", nameof(obj));
+            }
+            var otherCustomer = obj as CustomerForCompare;
+            return this.CompareTo(otherCustomer);
+
+        }
+
+        //-----------------------------------------------関係演算子(<,>,<=,>=)のオーバーロード
+        public static bool operator <(CustomerForCompare left, CustomerForCompare right)
+        {
+            return left.CompareTo(right) < 0;
+        }
+
+        public static bool operator <=(CustomerForCompare left, CustomerForCompare right)
+        {
+            return left.CompareTo(right) <= 0;
+        }
+
+        public static bool operator >(CustomerForCompare left, CustomerForCompare right)
+        {
+            return left.CompareTo(right) > 0;
+        }
+
+        public static bool operator >=(CustomerForCompare left, CustomerForCompare right)
+        {
+            return left.CompareTo(right) >= 0;
+        }
+
+        //-----------------------------------------------他メンバ変数の比較メソッド実装(Comparison利用)
+        //自作クラスに対して内部に定義して利用
+
+        /// <summary>
+        /// 比較メソッド()
+        /// </summary>
+        public static Comparison<CustomerForCompare> CompareByRevenue => (left, right) => left.revenue.CompareTo(right.revenue);
+
+        //-----------------------------------------------他メンバ変数の比較メソッド実装(IComparer利用)
+        //.NET内で定義された、ソースコードにアクセスできないクラスに対して、下記のクラスのみ定義して利用
+
+        /// <summary>
+        /// revenue比較のためだけの内部クラス
+        /// </summary>
+        private class RevenueComparer : IComparer<CustomerForCompare>
+        {
+            //-----------------------------------------------IComparable<Customer>の実装
+            //インスタンスの並べ替えを目的とする型固有の汎用比較メソッドをサポートするインタフェース(ジェネリクス)
+
+            /// <summary>
+            /// 比較
+            /// </summary>
+            /// <param name="left"></param>
+            /// <param name="right"></param>
+            /// <returns></returns>
+            int IComparer<CustomerForCompare>.Compare(CustomerForCompare left, CustomerForCompare right) => left.revenue.CompareTo(right.revenue);
+        }
+
+        /// <summary>
+        /// 遅延実行用にデリゲートを登録
+        /// Valueが呼び出された時だけ実行
+        /// </summary>
+        private static Lazy<RevenueComparer> revenueComparer = new Lazy<RevenueComparer>(() => new RevenueComparer());
+
+        /// <summary>
+        /// 外部公開用のインタフェース
+        /// </summary>
+        public static IComparer<CustomerForCompare> RevenueCompare => revenueComparer.Value;
+
+        
+
+
+
+    }
+}

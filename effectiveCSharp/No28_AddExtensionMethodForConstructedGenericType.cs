1c21d8f7b3b734370fe41d1999909faa6cf114a0
riko <c.c.3yegk1hass.w1s0vhd@gmail.com>
2020-06-17 22:54:10 +0900

    No.28

---
+++ effectiveCSharp/No28_AddExtensionMethodForConstructedGenericType.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using effectiveCSharp.Util;
+
+namespace effectiveCSharp
+{
+    /// <summary>
+    /// 構築された型に対する拡張メソッドを検討すること
+    /// </summary>
+    public class No28_AddExtensionMethodForConstructedGenericType
+    {
+        public No28_AddExtensionMethodForConstructedGenericType()
+        {
+            //目的：
+            //①さらにジェネリクスの利便性を高める
+
+            //概要：
+            //--------------------------------------------------------------------------------------
+            //特定の型引数が指定されたジェネリクス型（構築された型）に対して、特化したメソッドを定義したい場合、
+            //その構築された型にする拡張メソッドを定義する
+
+            //例えばSystem.Linq.Enumerableとかそう
+            var list = new List<int>() { 1, 2, 3, 4, 5 };
+            list.Sum();//これはIEnumerable<int>に対する拡張メソッド
+
+            //利用時
+            //良い例
+            var customers = new List<CustomerForGenerics>() { new CustomerForGenerics(), new CustomerForGenerics() , new CustomerForGenerics() };
+            //メソッドチェーン
+            //名前が「aaaa」の人にだけ５０％OFFクーポンを送る
+            customers.Where(v => v.Name == "aaaa").SendEmailCoupons("50%OFF!");
+            //30日間以上注文ない人たち
+            customers.LostProspects();
+            //インスタンス化しなくても利用可能
+            ConstructedGenericType.SendEmailCoupons(customers, "50%OFF!");
+
+            //悪い例
+            //ストレージモデルList<CustomerForGenerics>が必須
+            var customerlist = new CustomerList();
+            customerlist.Where(v => v.Name == "aaaa").SendEmailCoupons("50%OFF!");
+            customerlist.LostProspects();
+            //インスタンス化必須
+            //(No31)イテレータメソッドとの組み合わせが不可能
+        }
+    }
+}
---
+++ effectiveCSharp/Util/ConstructedGenericType.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace effectiveCSharp.Util
+{
+    //良い例
+    //拡張メソッドで追加
+    public static class ConstructedGenericType
+    {
+        //CustomerForGenericsコレクションからSendEmailCoupons()が実行できる
+        public static void SendEmailCoupons(this IEnumerable<CustomerForGenerics> cutstomers, string coupon)
+        {
+            //具体的な処理
+        }
+
+        //30日間注文がなかった顧客リストの取得
+        public static IEnumerable<CustomerForGenerics> LostProspects(this IEnumerable<CustomerForGenerics> target)
+        {
+            return target.Where(v => DateTime.Now - v.LastOrderDate > TimeSpan.FromDays(30));
+        }
+    }
+
+    //悪い例
+    //派生クラスをつくる
+    public class CustomerList : List<CustomerForGenerics>
+    {
+        public void SendEmailCoupons(string coupon)
+        {
+            //具体的な処理
+        }
+
+        public IEnumerable<CustomerForGenerics> LostProspects()
+        { 
+            return this.Where(v => DateTime.Now - v.LastOrderDate > TimeSpan.FromDays(30));
+        }
+    }
+
+
+    public class CustomerForGenerics
+    {
+        public string Name { get; set; }
+        public DateTime LastOrderDate { get; set; }
+    }
+}

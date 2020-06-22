e7e7be0d283c542c2d22e12a97323f76fa02064b
riko <c.c.3yegk1hass.w1s0vhd@gmail.com>
2020-06-23 00:27:11 +0900

    No.29

---
+++ effectiveCSharp/No29_ReturnIteratorNotCollection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using effectiveCSharp.Util;
+
+namespace effectiveCSharp
+{
+    /// <summary>
+    /// コレクションを返すメソッドではなくイテレータを返すメソッドとすること
+    /// </summary>
+    public class No29_ReturnIteratorNotCollection
+    {
+        public No29_ReturnIteratorNotCollection()
+        {
+            //目的：
+            //①シーケンス（IEnumerable<T>）の初期化・作成を便利に実現し、利用者に選択肢を与える
+
+            //概要：
+            //--------------------------------------------------------------------------------------
+            //集合を生成するメソッドを作成する場合、コレクション(List,Dictionary)でなく
+            //シーケンス（IEnumerable）返すメソッドをイテレータメソッドとして実装する
+            //そうすることで、利用者が任意のタイミングでコレクション化でき、性能劣化を引き起こさない
+
+            //利用時
+            foreach (var alph in Alphabets.Generate())
+            {
+                Console.WriteLine(alph);
+            }
+
+            //作成時にチェックを入れる
+            var sequence = Alphabets.GenerateSubSet('c', 'r');
+
+            //ここでエラーがでる
+            //var sequence = Alphabets.GenerateSubSet('c', 'a');
+
+            foreach (var alph in sequence)
+            {
+                Console.WriteLine(alph);
+            }
+
+        }
+    }
+}
---
+++ effectiveCSharp/Util/Alphabets.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace effectiveCSharp.Util
+{
+    /// <summary>
+    /// イテレータメソッドによる実装
+    /// </summary>
+    public class Alphabets
+    {
+        /// <summary>
+        /// 小文字でa~zまで作成する
+        /// シュガーシンタックス
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<char> Generate()
+        {
+            var letter = 'a';
+            while (letter <= 'z')
+            {
+                yield return letter;
+                letter++;
+            }
+        }
+
+        /// <summary>
+        /// 小文字でa~zまでの部分集合を作成する
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<char> GenerateSubSet(char first, char last)
+        {
+            //エラー
+            if (first < 'a')
+            {
+                throw new ArgumentException("1文字目はa以降にする必要があります", nameof(first));
+            }
+
+            if (first > 'z')
+            {
+                throw new ArgumentException("1文字目はz以前にする必要があります", nameof(first));
+            }
+
+            if (last < first)
+            {
+                throw new ArgumentException("最後の文字は1文字目より後方になければいけません", nameof(last));
+            }
+
+            if (first > 'z')
+            {
+                throw new ArgumentException("最後の文字はz以前にする必要があります", nameof(last));
+            }
+
+            //実際には上記のチェックはメソッド実行時にはされず、Iteratorによるアクセスを行った時にチェックされるため、
+            //foreachなどでアクセスした箇所でエラーをはく
+
+            //var letter = first;
+            //while (letter <= 'Z')
+            //{
+            //    yield return letter;
+            //    letter++;
+            //}
+
+            //そこでyieldブロックをローカル関数として切り出す
+            return GenerateSubsetImpl(first, last);
+        }
+
+        /// <summary>
+        /// ローカル関数
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="last"></param>
+        /// <returns></returns>
+        private static IEnumerable<char> GenerateSubsetImpl(char first, char last)
+        {
+            var letter = first;
+            while (letter <= last)
+            {
+                yield return letter;
+                letter++;
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// 上記のクラスからコンパイラが作成するクラス
+    /// Iteratorパターン（IEnumerableの実装）を自動生成している
+    /// </summary>
+    public class EmbeddedIterator : IEnumerable<char>
+    {
+        /// <summary>
+        /// メソッドの呼び出し
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<char> Generate() => new EmbeddedIterator();
+
+        /// <summary>
+        /// IEnumerable<char>の実装
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<char> GetEnumerator() => new LetterEnumerator();
+
+        /// <summary>
+        /// IEnumerableの実装
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator IEnumerable.GetEnumerator() => new LetterEnumerator();
+
+        /// <summary>
+        /// Enumeratorの本体
+        /// </summary>
+        private class LetterEnumerator : IEnumerator<char>
+        {
+            /// <summary>
+            /// letter
+            /// </summary>
+            private char letter = (char)('a' - 1);
+
+            /// <summary>
+            /// IEnumerator<char>の実装
+            /// Current
+            /// </summary>
+            public char Current => letter;
+
+            /// <summary>
+            /// IEnumeratorの実装
+            /// Current
+            /// </summary>
+            object IEnumerator.Current => letter;
+
+            /// <summary>
+            /// IEnumerator<char>の実装
+            /// MovableNext
+            /// </summary>
+            /// <returns></returns>
+            public bool MoveNext()
+            {
+                letter++;
+                return letter <= 'z';
+            }
+
+            /// <summary>
+            /// IEnumerator<char>の実装
+            /// Reset
+            /// </summary>
+            public void Reset()
+            {
+                letter = (char)('a' - 1);
+            }
+
+            /// <summary>
+            /// IEnumerator<char>の実装
+            /// Dispose
+            /// </summary>
+            public void Dispose()
+            {
+
+            }
+
+
+        }
+    }
+
+
+}

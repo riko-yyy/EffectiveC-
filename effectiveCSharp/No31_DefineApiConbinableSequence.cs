7737bffa197f8c6f87ac1aa7b5690fb8ed91cb48
riko <c.c.3yegk1hass.w1s0vhd@gmail.com>
2020-06-27 14:39:11 +0900

    No.31

---
+++ effectiveCSharp/No31_DefineApiConbinableSequence.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using effectiveCSharp.Util;
+namespace effectiveCSharp
+{
+    /// <summary>
+    /// シーケンス用の組み合わせ可能なAPIを作成する
+    /// </summary>
+    public class No31_DefineApiConbinableSequence
+    {
+        public No31_DefineApiConbinableSequence()
+        {
+            //目的：
+            //①遅延実行で組み合わせたクエリを１度のみ操作できる
+
+            //概要：
+            //--------------------------------------------------------------------------------------
+            //イテレータメソッド（yield return）を実装することで、
+            //再利用可能な遅延実行メソッドを実装できる
+
+            //利用時
+            var nums = new List<int>() { 1, 1, 2, 3, 3, 4, 5, 6, 7, 8 };
+            var list = new List<string>() { "a", "b", "a", "d", "c", "d", "j", "i", "d", "j", };
+
+            //NG
+            //処理が１つに内包されていて、再利用不可能
+            ProduceMethod.UniqueWrite(nums);
+
+            //OK
+            //イテレータメソッドとすることで再利用可能に
+            IteratorMethod.UniqueWrite(nums);
+
+            //さらにジェネリクスにすることで再利用可能に
+            var res = IteratorMethod.Unique(list);
+        }
+    }
+}
---
+++ effectiveCSharp/Util/IteratorMethod.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace effectiveCSharp.Util
+{
+    /// <summary>
+    /// 普通のやつ
+    /// </summary>
+    public class ProduceMethod
+    {
+        public static void UniqueWrite(IEnumerable<int> nums)
+        {
+            var uniqueVals = new HashSet<int>();
+            foreach (var num in nums)
+            {
+                if (!uniqueVals.Contains(num))
+                {
+                    uniqueVals.Add(num);
+                    Console.WriteLine(num * num);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// イテレータメソッド
+    /// </summary>
+    public class IteratorMethod
+    {
+        public static void UniqueWrite(IEnumerable<int> nums)
+        {
+            foreach (var num in Square(Unique(nums)))
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+        /// <summary>
+        /// 重複除去
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Unique<T>(IEnumerable<T> nums)
+        {
+            var uniqueVals = new HashSet<T>();
+            foreach (var num in nums)
+            {
+                if (!uniqueVals.Contains(num))
+                {
+                    uniqueVals.Add(num);
+                    yield return num;
+                }
+            }
+        }
+
+        /// <summary>
+        /// ２乗を返却
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static IEnumerable<int> Square(IEnumerable<int> nums)
+        {
+            foreach (var num in nums)
+            {
+                yield return num * num;
+            }
+        }
+    }
+
+
+}

78d0a6b2b1f9cf8819435493789c246799852a25
riko <c.c.3yegk1hass.w1s0vhd@gmail.com>
2020-07-02 22:43:57 +0900

    No.32

---
+++ effectiveCSharp/No32_SeparateAction,Predicate,FuncFromIterativeProcess.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using effectiveCSharp.Util;
+
+namespace effectiveCSharp
+{
+    /// <summary>
+    /// 反復処理をAction,Predicate,Funcと分離する
+    /// </summary>
+    public class No32_SeparateAction_Predicate_FuncFromIterativeProcess
+    {
+        public No32_SeparateAction_Predicate_FuncFromIterativeProcess()
+        {
+            //目的：
+            //①delegateを用いて、処理と走査を切り離し再利用可能なメソッドを定義する
+
+            //概要：
+            //--------------------------------------------------------------------------------------
+            //イテレータメソッド（yield return）は各要素を走査するものと各要素に処理を実行する２パターンに分類される
+            //delegateを利用することで任意の走査、処理を外部から与えることのできるメソッドを作成する
+
+            //利用時
+            var list = new List<int>() { 1, 2, 3, 4, 4, 6, 7, 2, 2, 7, 3, 4, 4, 5 };
+
+            //Predicate
+            var result1 = IteratorMethodUseDelegate.Where(list, v => v % 2 == 0);
+            var result2 = IteratorMethodUseDelegate.EveryNthItem(list, 3);
+
+            //Action
+            IteratorMethodUseDelegate.ForEach(list, v => Console.WriteLine(v));
+
+            //Func
+            var result3 = IteratorMethodUseDelegate.Select(list, v => v * v);
+
+
+            //delegateでもおk
+            //自クラスのメソッドを利用する場合
+            Predicate<int> predicate = IsTrue;
+            var result4 = IteratorMethodUseDelegate.Where(list, predicate);
+            //他クラスのメソッドを利用する場合
+            var a = new IteratorMethodUseDelegate();
+            predicate = a.IsTrue;
+            var result5 = IteratorMethodUseDelegate.Where(list, predicate);
+
+        }
+
+        public bool IsTrue(int item)
+        {
+            return true;
+        }
+    }
+}
---
+++ effectiveCSharp/Util/IteratorMethodUseDelegate.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace effectiveCSharp.Util
+{
+    public class IteratorMethodUseDelegate
+    {
+        #region Predicate   
+        /// <summary>
+        /// フィルタ
+        /// </summary>
+        /// <typeparam name="T">要素の型</typeparam>
+        /// <param name="sequence">シーケンス</param>
+        /// <param name="filterFunc">フィルタPredicate</param>
+        /// <returns></returns>
+        public static IEnumerable<T> Where<T>(IEnumerable<T> sequence, Predicate<T> filterFunc)
+        {
+            if (sequence == null)
+            {
+                throw new ArgumentException("シーケンスをnullにできません", nameof(sequence));
+            }
+
+            if (filterFunc == null)
+            {
+                throw new ArgumentException("述語をnullにできません", nameof(filterFunc));
+            }
+
+            foreach (T item in sequence)
+            {
+                if (filterFunc(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        /// <summary>
+        /// シーケンスのN番目ごとの要素を返す
+        /// </summary>
+        /// <typeparam name="T">要素の型</typeparam>
+        /// <param name="sequence">シーケンス</param>
+        /// <param name="period">N</param>
+        /// <returns></returns>
+        public static IEnumerable<T> EveryNthItem<T>(IEnumerable<T> sequence, int period)
+        {
+            if (sequence == null)
+            {
+                throw new ArgumentException("シーケンスをnullにできません", nameof(sequence));
+            }
+
+            if (period > 0)
+            {
+                throw new ArgumentException("Nは0より大きい整数である必要があります", nameof(period));
+            }
+
+            var count = 0;
+
+            foreach (T item in sequence)
+            {
+                if (++count % period == 0)
+                {
+                    yield return item;
+                }
+
+            }
+        }
+        #endregion
+
+        #region Action
+        /// <summary>
+        /// シーケンスの各要素に対して処理を実行する
+        /// </summary>
+        /// <typeparam name="T">要素の型</typeparam>
+        /// <param name="sequence">シーケンス</param>
+        /// <param name="action">処理</param>
+        public static void ForEach<T>(IEnumerable<T> sequence, Action<T> action)
+        {
+            if (sequence == null)
+            {
+                throw new ArgumentException("シーケンスをnullにできません", nameof(sequence));
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentException("処理をnullにできません", nameof(action));
+            }
+
+            foreach (T item in sequence)
+            {
+                action(item);
+            }
+        }
+        #endregion
+
+        #region Func       
+        /// <summary>
+        /// シーケンスの各要素を加工し、そのシーケンスを返す
+        /// </summary>
+        /// <typeparam name="Tin">加工前の要素の型</typeparam>
+        /// <typeparam name="Tout">加工後の要素の型</typeparam>
+        /// <param name="sequence">シーケンス</param>
+        /// <param name="method">加工メソッド</param>
+        /// <returns></returns>
+        public static IEnumerable<Tout> Select<Tin,Tout>(IEnumerable<Tin> sequence, Func<Tin,Tout> method)
+        {
+            if (sequence == null)
+            {
+                throw new ArgumentException("シーケンスをnullにできません", nameof(sequence));
+            }
+
+            if (method == null)
+            {
+                throw new ArgumentException("関数をnullにできません", nameof(method));
+            }
+
+            foreach(Tin item in sequence)
+            {
+                yield return method(item);
+            }
+        }
+        #endregion
+
+
+        public bool IsTrue(int num)
+        {
+            return true;
+        }
+    }
+}

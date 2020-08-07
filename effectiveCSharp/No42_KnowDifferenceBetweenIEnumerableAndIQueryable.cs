ed51ca5c9983b968acf79e23da621e76ff35ae64
riko <c.c.3yegk1hass.w1s0vhd@gmail.com>
2020-08-07 22:42:19 +0900

    No.42

---
+++ effectiveCSharp/No42_KnowDifferenceBetweenIEnumerableAndIQueryable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using effectiveCSharp.Util;
+
+namespace effectiveCSharp
+{
+    /// <summary>
+    /// IEnumerableとIQueryableを区別すること
+    /// </summary>
+    public class No42_KnowDifferenceBetweenIEnumerableAndIQueryable
+    {
+        public No42_KnowDifferenceBetweenIEnumerableAndIQueryable()
+        {
+            //目的：
+            //IEnumerableとIQueryableの違いを把握し、データソースに適したアクセスを行う
+
+            //概要：
+            //--------------------------------------------------------------------------------------
+            //IEnumerableはデリゲートで扱うのに対し、IQueryableは式ツリーとして扱う
+            //これにより、IEnumerableはローカルで処理されるのに対し、IQueryableは式ツリーはクエリとして解釈されその結果を取得する
+            //基本使用はIQueryableを推奨
+
+            //例
+            var allEmployees = new List<Employee>() { new Employee() { Name = "a", Age = 1, Salary = 1 }, new Employee() { Name = "b", Age = 2, Salary = 2 }, new Employee() { Name = "c", Age = 3, Salary = 3 }, };
+
+            //別物
+            var over1 = from e in allEmployees.AsEnumerable()
+                        where e.Age > 1
+                        where e.Salary > 1
+                        select e;
+
+            var over2 = from e in allEmployees.AsQueryable()
+                        where e.Age > 1
+                        where e.Salary > 1
+                        select e;
+
+            //IQueryableの場合、クエリに解釈されるのでクエリに変換できるメソッドのみLinq中で利用できる
+            //動作する
+            var over3 = from e in allEmployees.AsEnumerable()
+                        where IsOverOneAge(e)
+                        select e;
+            //例外となる
+            var over4 = from e in allEmployees.AsQueryable()
+                        where IsOverOneAge(e)
+                        select e;
+
+
+        }
+
+        private bool IsOverOneAge(Employee emp)
+        {
+            return emp.Age > 1;
+        }
+    }
+}
--- effectiveCSharp/Util/ExtensionLinq.cs
+++ effectiveCSharp/Util/ExtensionLinq.cs
@@ -4,13 +4,16 @@ using System.Linq;
 
 namespace effectiveCSharp.Util
 {
+    //データソースに応じてどちらのメソッドを実装するかは決定する
+    //両方実装する場合は以下のようにするとよい
     public static class ExtensionLinq
     {
         public static IEnumerable<Employee> OverOneAge(this IEnumerable<Employee> seq)
         {
-            return from e in seq
-                   where e.Age > 1
-                   select e;
+            //AsQueryable()はシーケンスがIQueryableを実装している場合、シーケンスをIQueryableに変換して返却し
+            //実装していない場合、IQueryableを実装するラッパーを返却する
+            //つまりIQueryableが利用できれば利用し、できなければIEnumerableとして動作する
+            return OverOneAge(seq.AsQueryable());
         }
 
         public static IQueryable<Employee> OverOneAge(this IQueryable<Employee> seq)
